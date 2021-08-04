e12e6f9ad1884161c7b14240ac0886f2626dff6c
garceling <86638178+garceling@users.noreply.github.com>
2021-08-04 11:05:40 -0400

    Update parameters.py

--- User_Camera/Drowsiness_Detector/parameters.py
+++ User_Camera/Drowsiness_Detector/parameters.py
@@ -1,1 +1,11 @@
+import os
+
+shape_predictor_path    = os.path.join('shape_predictor_68_face_landmarks.dat')
+
+
+EYE_DROWSINESS_THRESHOLD    = 0.25
+EYE_DROWSINESS_INTERVAL     = 2.0
+MOUTH_DROWSINESS_THRESHOLD  = 0.37
+MOUTH_DROWSINESS_INTERVAL   = 1.0
+DISTRACTION_INTERVAL        = 3.0
 
