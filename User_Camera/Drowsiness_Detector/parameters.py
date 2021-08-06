363eb718bb7f5ecba26f2c9dca7348408d795fc6
garceling <86638178+garceling@users.noreply.github.com>
2021-08-06 00:37:38 -0400

    Update parameters.py

--- User_Camera/Drowsiness_Detector/parameters.py
+++ User_Camera/Drowsiness_Detector/parameters.py
@@ -3,7 +3,7 @@ import os
 shape_predictor_path    = os.path.join('/home/pi/tf/Front Camera/yawn/', 'shape_predictor_68_face_landmarks.dat')
 
 
-EYE_DROWSINESS_THRESHOLD    = 0.25
+EYE_DROWSINESS_THRESHOLD    = 0.20
 EYE_DROWSINESS_INTERVAL     = 2.0
 MOUTH_DROWSINESS_THRESHOLD  = 0.37
 MOUTH_DROWSINESS_INTERVAL   = 1.0
