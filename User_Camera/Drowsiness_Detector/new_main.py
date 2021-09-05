342205f7034476a110ae7ae45d4e62c04a65d29f
garceling <86638178+garceling@users.noreply.github.com>
2021-09-05 17:41:42 -0400

    Update new_main.py

--- User_Camera/Drowsiness_Detector/new_main.py
+++ User_Camera/Drowsiness_Detector/new_main.py
@@ -8,8 +8,9 @@ import dlib
 import cv2
 from datetime import datetime
 
-
+#draw a bounding box over face
 def get_max_area_rect(rects):
+    # checks to see if a face was not dectected (0)
     if len(rects)==0: return
     areas=[]
     for rect in rects:
@@ -47,11 +48,14 @@ def facial_processing():
     detector    = dlib.get_frontal_face_detector()
     predictor   = dlib.shape_predictor('/home/pi/tflite1/Drowsiness_Detector/shape_predictor_68_face_landmarks.dat')
 
+    # grab the indexes of the facial landmarks for the left and right eye, respectively
     ls,le = face.FACIAL_LANDMARKS_IDXS["left_eye"]
     rs,re = face.FACIAL_LANDMARKS_IDXS["right_eye"]
 
+    #start video stream
     cap=cv2.VideoCapture(0)
-
+    
+    #count the fps
     fps_counter=0
     fps_to_display='initializing...'
     fps_timer=time.time()
@@ -59,6 +63,7 @@ def facial_processing():
     while True:
         _ , frame=cap.read()
         fps_counter+=1
+	#flip around y-axis
         frame = cv2.flip(frame, 1)
         if time.time()-fps_timer>=1.0:
             fps_to_display=fps_counter
@@ -68,12 +73,14 @@ def facial_processing():
         cv2.putText(frame, "FPS :"+str(fps_to_display), (frame.shape[1]-100, frame.shape[0]-10),\
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-	#resize frame
-        #frame = imutils.resize(frame, width=900)
+
 	#convert frame to grayscale
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+	
+	
+	# detect faces in the grayscale frame
         rects = detector(gray, 0)
+	#draw bounding box on face
         rect=get_max_area_rect(rects)
 
 
@@ -82,14 +89,17 @@ def facial_processing():
             if distracton_initialized==True:
                 interval=time.time()-distracton_start_time
                 interval=str(round(interval,3))
+		#gets the current date/time
                 dateTime= datetime.now()
                 distracton_initialized=False
                 info="Date: " + str(dateTime) + ", Interval: " + interval + ", Type: Eyes not on road"
                 info=info+ "\n"
                 if time.time()- distracton_start_time> DISTRACTION_INTERVAL:
+		   #stores the info into a txt file
                     with open(r'/home/pi/Desktop/output.txt', "a+") as file_object:
                         file_object.write(info)
-
+			
+	    # determine the facial landmarks for the face region, then convert the facial landmark (x, y)-coordinates to a NumPy array
             shape = predictor(gray, rect)
             shape = face.shape_to_np(shape)
 		
@@ -97,6 +107,7 @@ def facial_processing():
 	    # coordinates to compute the eye aspect ratio for both eyes
             leftEye = shape[ls:le]
             rightEye = shape[rs:re]
+	    #gets the EAR for each eye
             leftEAR = get_eye_aspect_ratio(leftEye)
             rightEAR = get_eye_aspect_ratio(rightEye)
 
@@ -130,9 +141,9 @@ def facial_processing():
                     cv2.putText(frame, "YOU ARE DROWSY!", (10, 30),
                                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
                     #uncomment the three lines below to help check the validity of the program
-                    dateTimeOBJ=datetime.now()
-                    eye_info="Date: " + str(dateTimeOBJ) + " Interval: " + str(time.time()-eye_start_time ) + " Drowsy"
-                    print(eye_info)
+                    #dateTimeOBJ=datetime.now()
+                    #eye_info="Date: " + str(dateTimeOBJ) + " Interval: " + str(time.time()-eye_start_time ) + " Drowsy"
+                    #print(eye_info)
 
             else:
                 #measures the duration where the users eyes were drowsy
@@ -143,7 +154,9 @@ def facial_processing():
                     eye_initialized=False
                     info_eye="Date: " + str(dateTime_eye) + ", Interval: " + interval_eye + ", Type:Drowsy"
                     info_eye=info_eye+ "\n"
+		    ##will only store the info if user eyes close/droop for a sufficient amount of time
                     if time.time()-eye_start_time >= EYE_DROWSINESS_INTERVAL:
+			#store info into a txt file
                         with open(r'/home/pi/Desktop/output.txt', "a+") as file_object:
                             file_object.write(info_eye)
 
@@ -160,9 +173,9 @@ def facial_processing():
                     cv2.putText(frame, "YOU ARE YAWNING!", (10, 70),
                                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
                     #uncomment the lines below to check the validity of this program
-                    dateTimeOBJ2=datetime.now()
-                    mouth_info="date: " + str(dateTimeOBJ2) + " Interval: " + str(time.time()-mouth_start_time ) + " Yawning" + " mar " + str(mar)
-                    print(mouth_info)
+                    #dateTimeOBJ2=datetime.now()
+                    #mouth_info="date: " + str(dateTimeOBJ2) + " Interval: " + str(time.time()-mouth_start_time ) + " Yawning" + " mar " + str(mar)
+                    #print(mouth_info)
 
             else:
                 #measures duration of users yawn
@@ -173,7 +186,9 @@ def facial_processing():
                     mouth_initialized=False
                     info_mouth="Date: " + str(dateTime_mouth) + ", Interval: " + interval_mouth + ", Type:Yawning"
                     info_mouth=info_mouth+ "\n"
+		    #will only store the info if user yawns for a sufficient amount of time
                     if time.time()-mouth_start_time >= MOUTH_DROWSINESS_INTERVAL:
+			#store into into a txt file
                         with open(r'/home/pi/Desktop/output.txt', "a+") as file_object:
                             file_object.write(info_mouth)
 
@@ -198,6 +213,7 @@ def facial_processing():
                     normal_initialized=False
                     info_normal="Date: " + str(dateTime_normal) + ", Interval: " + interval_normal+ ", Type:Normal"
                     info_normal=info_normal+ "\n"
+		    #will only store the info if user is focused for a sufficient amount of time
                     if time.time()-normal_start_time >= NORMAL_INTERVAL:
                         with open(r'/home/pi/Desktop/output.txt', "a+") as file_object:
                             file_object.write(info_normal)
@@ -205,7 +221,7 @@ def facial_processing():
 
 	#if the user's face is not focused on the road, the eyes/mouth features cannot be computed
         else:
-            #I added this here, if the user ever turns their head so that there eyes are no longer on the road
+            #I added this here, if the user ever turns their head so that their eyes are no longer on the road
             #but at the same time their eyes were drowsy at first, this resets the timer for the drowsiness detector
             if eye_initialized==True:
                     interval_eye=time.time()-eye_start_time
@@ -224,20 +240,23 @@ def facial_processing():
                 #eye_initialized=False
 	    #checks if the user's eyes are off the road after a sufficient number of frames
             if time.time()- distracton_start_time> DISTRACTION_INTERVAL:
-
+		#displays on screen that the driver's eyes are off the road
                 cv2.putText(frame, "PLEASE KEEP EYES ON ROAD", (10, 30),
                             cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
                 #uncomment the line below if want to check the validity of the program
-                dateTimeOBJ3=datetime.now()
-                DIST_info="date: " + str(dateTimeOBJ3) + " Interval: " + str(time.time()-distracton_start_time) + " EYES NOT ON ROAD"
-                print(DIST_info)
+                #dateTimeOBJ3=datetime.now()
+                #DIST_info="date: " + str(dateTimeOBJ3) + " Interval: " + str(time.time()-distracton_start_time) + " EYES NOT ON ROAD"
+                #print(DIST_info)
 
 	#show the frame
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(5)&0xFF
+	
+	# if the `q` key was pressed, break from the loop
         if key == ord("q"):
             break
-
+	
+    # close all windows and release the capture
     cv2.destroyAllWindows()
     cap.release()
 
